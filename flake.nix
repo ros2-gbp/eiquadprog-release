395abacfc62769f44f75c76cbddcad0abd3ce81c
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-14 20:03:09 +0200

    Rebase from 'release/kilted/eiquadprog'

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "C++ reimplementation of eiquadprog";
+
+  inputs = {
+    gepetto.url = "github:gepetto/nix";
+    flake-parts.follows = "gepetto/flake-parts";
+    nixpkgs.follows = "gepetto/nixpkgs";
+    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    systems.follows = "gepetto/systems";
+    treefmt-nix.follows = "gepetto/treefmt-nix";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.gepetto.flakeModule ];
+      perSystem =
+        {
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
+        {
+          packages = {
+            default = self'.packages.eiquadprog;
+            eiquadprog = pkgs.eiquadprog.overrideAttrs (_: {
+              src = pkgs.lib.fileset.toSource {
+                root = ./.;
+                fileset = pkgs.lib.fileset.unions [
+                  ./CMakeLists.txt
+                  ./include
+                  ./package.xml
+                  ./src
+                  ./tests
+                ];
+              };
+            });
+          };
+        };
+    };
+}
